96b0c45ac98f06ccfda238776c58b79e5f9df587
Geunseok Byeon <byeongeunseok@gmail.com>
2024-03-12 18:55:23 +0900

    Change plugin of file brower

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,7 +19,14 @@ require("lazy").setup({
         end,
     },
     {
-        "preservim/nerdtree",
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
     },
     {
         "nvim-treesitter/nvim-treesitter",
@@ -44,69 +51,69 @@ require("lazy").setup({
 
             lspconfig.clangd.setup {}
             -- lspconfig.rust_analyzer.setup {
-                --     settings = {
-                    --         ["rust-analyzer"] = {},
-                    --     },
-                    -- }
-                    lspconfig.tsserver.setup {}
-                    lspconfig.pyright.setup {}
-                    lspconfig.lua_ls.setup {}
-                end,
-            },
-            {
-                "lukas-reineke/indent-blankline.nvim",
-                main = "ibl",
-                opts = {
+            --     settings = {
+            --         ["rust-analyzer"] = {},
+            --     },
+            -- }
+            lspconfig.tsserver.setup {}
+            lspconfig.pyright.setup {}
+            lspconfig.lua_ls.setup {}
+        end,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {
+        },
+        config = function()
+            local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+
+            local hooks = require "ibl.hooks"
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+
+            require("ibl").setup { indent = { highlight = highlight } }
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup {
+                signs = {
+                    add          = { text = '│' },
+                    change       = { text = '│' },
+                    delete       = { text = '_' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
                 },
-                config = function()
-                    local highlight = {
-                        "RainbowRed",
-                        "RainbowYellow",
-                        "RainbowBlue",
-                        "RainbowOrange",
-                        "RainbowGreen",
-                        "RainbowViolet",
-                        "RainbowCyan",
-                    }
+            }
+        end,
+    },
+    {
+        "numToStr/Comment.nvim",
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+    },
 
-                    local hooks = require "ibl.hooks"
-                    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-                        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-                        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-                        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-                        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-                        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-                        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-                        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-                    end)
-
-                    require("ibl").setup { indent = { highlight = highlight } }
-                end,
-            },
-            {
-                "tpope/vim-fugitive",
-            },
-            {
-                "lewis6991/gitsigns.nvim",
-                config = function()
-                    require("gitsigns").setup {
-                        signs = {
-                            add          = { text = '│' },
-                            change       = { text = '│' },
-                            delete       = { text = '_' },
-                            topdelete    = { text = '‾' },
-                            changedelete = { text = '~' },
-                            untracked    = { text = '┆' },
-                        },
-                    }
-                end,
-            },
-            {
-                "numToStr/Comment.nvim",
-                opts = {
-                    -- add any options here
-                },
-                lazy = false,
-            },
-
-        })
+})
