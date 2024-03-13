a2a404ad171c26e99327c2856dc67222d1d504eb
Geunseok Byeon <byeongeunseok@gmail.com>
2024-03-14 00:21:40 +0900

    Add lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -50,11 +50,6 @@ require("lazy").setup({
             local lspconfig = require("lspconfig")
 
             lspconfig.clangd.setup {}
-            -- lspconfig.rust_analyzer.setup {
-            --     settings = {
-            --         ["rust-analyzer"] = {},
-            --     },
-            -- }
             lspconfig.tsserver.setup {}
             lspconfig.pyright.setup {}
             lspconfig.lua_ls.setup {}
@@ -98,12 +93,12 @@ require("lazy").setup({
         config = function()
             require("gitsigns").setup {
                 signs = {
-                    add          = { text = '│' },
-                    change       = { text = '│' },
-                    delete       = { text = '_' },
-                    topdelete    = { text = '‾' },
-                    changedelete = { text = '~' },
-                    untracked    = { text = '┆' },
+                    add          = { text = "│" },
+                    change       = { text = "│" },
+                    delete       = { text = "_" },
+                    topdelete    = { text = "‾" },
+                    changedelete = { text = "~" },
+                    untracked    = { text = "┆" },
                 },
             }
         end,
@@ -115,5 +110,50 @@ require("lazy").setup({
         },
         lazy = false,
     },
-
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup {
+                options = {
+                    icons_enabled = true,
+                    theme = "auto",
+                    component_separators = { left = "", right = ""},
+                    section_separators = { left = "", right = ""},
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 1000,
+                        tabline = 1000,
+                        winbar = 1000,
+                    }
+                },
+                sections = {
+                    lualine_a = {"mode"},
+                    lualine_b = {"branch", "diff", "diagnostics"},
+                    lualine_c = {"filename"},
+                    lualine_x = {"encoding", "fileformat", "filetype"},
+                    lualine_y = {"progress"},
+                    lualine_z = {"location"}
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {"filename"},
+                    lualine_x = {"location"},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+                extensions = {}
+            }
+        end,
+    }
 })
